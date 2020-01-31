3506d4f69fca6c5a2ed8f60fa089095211328849
Jun Lee <jy45lee@gmail.com>
2020-01-31 01:41:04 -0800

    adding logo

--- github_followers/SearchViewController.swift
+++ github_followers/SearchViewController.swift
@@ -9,10 +9,32 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    let logoImageView = UIImageView()
+    let userNameText = GFTextField()
+    let searchButton = GFButton(backgroundColor: .systemGreen, title: "Search Followers")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground // set background according to light/dark mode
+        configureLogoImageView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView(){
+        view.addSubview(logoImageView) // eq. to adding image view in storyboard. won't show up without this line
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")!
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80), // 80 is from trial/error
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
