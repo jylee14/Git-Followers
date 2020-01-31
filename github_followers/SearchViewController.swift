17c39a19ed79106a2ba6f10232b6600886974087
Jun Lee <jy45lee@gmail.com>
2020-01-31 01:55:29 -0800

    finished layout of search page

--- github_followers/SearchViewController.swift
+++ github_followers/SearchViewController.swift
@@ -18,6 +18,8 @@ class SearchViewController: UIViewController {
 
         view.backgroundColor = .systemBackground // set background according to light/dark mode
         configureLogoImageView()
+        configureTextField()
+        configureSearchButton()
     }
     
     override func viewWillAppear(_ animated: Bool){
@@ -35,6 +37,28 @@ class SearchViewController: UIViewController {
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField(){
+        view.addSubview(userNameText)
+        
+        NSLayoutConstraint.activate([
+            userNameText.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            userNameText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            userNameText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            userNameText.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureSearchButton(){
+        view.addSubview(searchButton)
+        
+        NSLayoutConstraint.activate([
+            searchButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -75),
+            searchButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            searchButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            searchButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
