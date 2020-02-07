e72e976a7417ebd184a66cf804467148f36e855a
Jun Lee <jy45lee@gmail.com>
2020-02-06 22:13:37 -0800

    organizing project

---
+++ github_followers/CustomViews/GFAlertView.swift
@@ -0,0 +1,21 @@
+//
+//  GFAlertView.swift
+//  github_followers
+//
+//  Created by Jun Lee on 2/6/20.
+//  Copyright © 2020 Jun Lee. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
